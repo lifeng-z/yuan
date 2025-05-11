4476cfed125aac78eef129c740a77778ce741c8c
lifeng-z <yuan@lifeng-.com>
2025-05-11 19:36:26 +0800

    test

---
+++ 202312473-张利锋/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zlf on 2025/5/11.
+//
+
+#include "test.h"
+#include "iostream"
+using namespace std;
+int main(){
+    cout<<1;
+}
---
+++ 202312473-张利锋/test.h
@@ -0,0 +1,14 @@
+//
+// Created by zlf on 2025/5/11.
+//
+
+#ifndef YUAN_TEST_H
+#define YUAN_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //YUAN_TEST_H
